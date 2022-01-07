da96edb5a2579962daed18df65388321fa8d9b19
Ezeki <ez@jok.io>
2022-01-07 12:30:20 +0400

    feat: add integration test

--- e2e/integration.test.ts
+++ e2e/integration.test.ts
@@ -4,7 +4,6 @@ import * as path from "path";
 var devnet = new StacksDevnetOrchestrator({
   path: path.join(__dirname, "../Clarinet.toml"),
   logs: true,
-  devnet: {},
 });
 
 beforeAll(() => devnet.start());
@@ -13,8 +12,6 @@ afterAll(() => devnet.stop());
 test("build the block", async () => {
   const nodeUrl = devnet.getStacksNodeUrl();
   expect(nodeUrl).toBeTruthy();
-
-  console.log("nodeUrl", nodeUrl);
 
   const block = await devnet.waitForStacksBlock();
 
